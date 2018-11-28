bf973981043bded15ab7c6aedec40d1aa85cef4d
bourne <03330130@163.com>
2018-11-28 10:46:52 +0800

    1111111

---
+++ app/src/main/java/bourne/com/weatherdemo/db/Country.java
@@ -0,0 +1,42 @@
+package bourne.com.weatherdemo.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int countryId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+}
